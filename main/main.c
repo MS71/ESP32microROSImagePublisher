ab54e65f2fb34c05e7278dc896fb8bd6ef9af831
MS71 <MS71>
2021-12-05 21:50:49 +0100

    -

--- main/main.c
+++ main/main.c
@@ -18,19 +18,130 @@
 #include <rmw_microros/rmw_microros.h>
 #endif
 
+#include <micro_ros_utilities/type_utilities.h>
+#include <micro_ros_utilities/string_utilities.h>
+
+#include <sensor_msgs/msg/image.h>
+
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
+#include "esp_camera.h"
+
+#define BOARD_ESP32CAM_AITHINKER 1
+
+// WROVER-KIT PIN Map
+#ifdef BOARD_WROVER_KIT
+
+#define CAM_PIN_PWDN -1  //power down is not used
+#define CAM_PIN_RESET -1 //software reset will be performed
+#define CAM_PIN_XCLK 21
+#define CAM_PIN_SIOD 26
+#define CAM_PIN_SIOC 27
+
+#define CAM_PIN_D7 35
+#define CAM_PIN_D6 34
+#define CAM_PIN_D5 39
+#define CAM_PIN_D4 36
+#define CAM_PIN_D3 19
+#define CAM_PIN_D2 18
+#define CAM_PIN_D1 5
+#define CAM_PIN_D0 4
+#define CAM_PIN_VSYNC 25
+#define CAM_PIN_HREF 23
+#define CAM_PIN_PCLK 22
+
+#endif
+
+// ESP32Cam (AiThinker) PIN Map
+#ifdef BOARD_ESP32CAM_AITHINKER
+
+#define CAM_PIN_PWDN 32
+#define CAM_PIN_RESET -1 //software reset will be performed
+#define CAM_PIN_XCLK 0
+#define CAM_PIN_SIOD 26
+#define CAM_PIN_SIOC 27
+
+#define CAM_PIN_D7 35
+#define CAM_PIN_D6 34
+#define CAM_PIN_D5 39
+#define CAM_PIN_D4 36
+#define CAM_PIN_D3 21
+#define CAM_PIN_D2 19
+#define CAM_PIN_D1 18
+#define CAM_PIN_D0 5
+#define CAM_PIN_VSYNC 25
+#define CAM_PIN_HREF 23
+#define CAM_PIN_PCLK 22
+
+#endif
+
+static const char *TAG = "example:take_picture";
+
+static camera_config_t camera_config = {
+    .pin_pwdn = CAM_PIN_PWDN,
+    .pin_reset = CAM_PIN_RESET,
+    .pin_xclk = CAM_PIN_XCLK,
+    .pin_sscb_sda = CAM_PIN_SIOD,
+    .pin_sscb_scl = CAM_PIN_SIOC,
+
+    .pin_d7 = CAM_PIN_D7,
+    .pin_d6 = CAM_PIN_D6,
+    .pin_d5 = CAM_PIN_D5,
+    .pin_d4 = CAM_PIN_D4,
+    .pin_d3 = CAM_PIN_D3,
+    .pin_d2 = CAM_PIN_D2,
+    .pin_d1 = CAM_PIN_D1,
+    .pin_d0 = CAM_PIN_D0,
+    .pin_vsync = CAM_PIN_VSYNC,
+    .pin_href = CAM_PIN_HREF,
+    .pin_pclk = CAM_PIN_PCLK,
+
+    //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_RGB565, //YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_QVGA,    //QQVGA-UXGA Do not use sizes above QVGA when not JPEG
+
+    .jpeg_quality = 12, //0-63 lower number means higher quality
+    .fb_count = 1,       //if more than one, i2s runs in continuous mode. Use only with JPEG
+    .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+};
+
+static esp_err_t init_camera()
+{
+    //initialize the camera
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Camera Init Failed");
+        return err;
+    }
+
+    return ESP_OK;
+}
+
 rcl_publisher_t publisher;
-std_msgs__msg__Int32 msg;
+sensor_msgs__msg__Image msg;
+sensor_msgs__msg__Image msg_static;
+
+uint8_t my_buffer[1000];
 
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 {
 	RCLC_UNUSED(last_call_time);
 	if (timer != NULL) {
-		printf("Publishing: %d\n", msg.data);
+        	ESP_LOGI(TAG, "Taking picture...");
+        	camera_fb_t *pic = esp_camera_fb_get();
+
+        	// use pic->buf to access the image
+        	ESP_LOGI(TAG, "Picture taken! Its size was: %zu bytes", pic->len);
+        	esp_camera_fb_return(pic);
+
 		RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-		msg.data++;
+		RCSOFTCHECK(rcl_publish(&publisher, &msg_static, NULL));
 	}
 }
 
@@ -42,8 +153,6 @@ void micro_ros_task(void * arg)
 	rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
 	RCCHECK(rcl_init_options_init(&init_options, allocator));
 
-        printf("micro_ros_task ...\n");
-
 #ifdef CONFIG_MICRO_ROS_ESP_XRCE_DDS_MIDDLEWARE
 	rmw_init_options_t* rmw_options = rcl_init_options_get_rmw_init_options(&init_options);
 
@@ -51,28 +160,19 @@ void micro_ros_task(void * arg)
 	RCCHECK(rmw_uros_options_set_udp_address(CONFIG_MICRO_ROS_AGENT_IP, CONFIG_MICRO_ROS_AGENT_PORT, rmw_options));
 	//RCCHECK(rmw_uros_discover_agent(rmw_options));
 #endif
-
-        printf("micro_ros_task ...\n");
-
 	// create init_options
 	RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
 
-        printf("micro_ros_task ...\n");
-
 	// create node
 	rcl_node_t node;
-	RCCHECK(rclc_node_init_default(&node, "esp32_int32_publisher", "", &support));
-
-        printf("micro_ros_task ...\n");
+	RCCHECK(rclc_node_init_default(&node, "esp32_node", "", &support));
 
 	// create publisher
 	RCCHECK(rclc_publisher_init_default(
 		&publisher,
 		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-		"freertos_int32_publisher"));
-
-        printf("micro_ros_task ...\n");
+    	ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Image),
+		"esp32_publisher"));
 
 	// create timer,
 	rcl_timer_t timer;
@@ -83,17 +183,106 @@ void micro_ros_task(void * arg)
 		RCL_MS_TO_NS(timer_timeout),
 		timer_callback));
 
-        printf("micro_ros_task ...\n");
-
 	// create executor
 	rclc_executor_t executor;
 	RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
 	RCCHECK(rclc_executor_add_timer(&executor, &timer));
 
-	msg.data = 0;
+	// INIT MESSAGE MEMORY
 
-        printf("micro_ros_task ... loop ...\n");
+	// --- Configuration ---
 
+	// micro-ROS utilities allows to configure the dynamic memory initialization using a micro_ros_utilities_memory_conf_t structure
+	// If some member of this struct is set to zero, the library will use the default value.
+	// Check `micro_ros_utilities_memory_conf_default` in `<micro_ros_utilities/type_utilities.h>` for those defaults.
+
+	static micro_ros_utilities_memory_conf_t conf = {0};
+
+	// OPTIONALLY this struct can configure the default size of strings, basic sequences and composed sequences
+
+	conf.max_string_capacity = 50;
+	conf.max_ros2_type_sequence_capacity = 5;
+	conf.max_basic_type_sequence_capacity = 5;
+
+	// OPTIONALLY this struct can store rules for specific members
+	// !! Using the API with rules will use dynamic memory allocations for handling strings !!
+
+	micro_ros_utilities_memory_rule_t rules[] = {
+		{"header.frame_id", 30},
+		{"encoding", 3},
+		{"data", 400}
+	};
+	conf.rules = rules;
+	conf.n_rules = sizeof(rules) / sizeof(rules[0]);
+
+	// --- API ---
+	// It can be calculated the size that will be needed by a msg with a certain configuration
+
+	size_t dynamic_size = micro_ros_utilities_get_dynamic_size(
+		ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Image),
+		conf
+	);
+
+	// The total (stack, static & dynamic) memory usage of a packet will be:
+
+	size_t message_total_size = dynamic_size + sizeof(sensor_msgs__msg__Image);
+
+	// The message dynamic memory can be allocated using the following call.
+	// This will use rcutils default allocators for getting memory.
+
+	bool success = micro_ros_utilities_create_message_memory(
+		ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Image),
+		&msg,
+		conf
+	);
+
+	// The message dynamic memory can also be allocated using a buffer.
+	// This will NOT use dynamic memory for the allocation.
+	// If no rules set in the conf, no dynamic allocation is guaranteed.
+	// This method will use contiguos memory and will not take into account aligment
+	// so handling statically allocated msg can be less efficient that dynamic ones
+
+	size_t static_size = micro_ros_utilities_get_static_size(
+		ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Image),
+		conf
+	);
+
+	message_total_size = static_size + sizeof(sensor_msgs__msg__Image);
+
+	// my_buffer should have at least static_size Bytes
+	success &= micro_ros_utilities_create_static_message_memory(
+		ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Image),
+		&msg_static,
+		conf,
+		my_buffer,
+		sizeof(my_buffer)
+	);
+
+	// Dynamically allocated messages can be destroyed using:
+
+	// success &= micro_ros_utilities_destroy_message_memory(
+	//   ROSIDL_GET_MSG_TYPE_SUPPORT(control_msgs, msg, JointJog),
+	//   &msg,
+	//   conf
+	// );
+
+	// Fill the message
+	msg.header.frame_id = micro_ros_string_utilities_set(msg.header.frame_id, "myframe");
+	msg_static.header.frame_id = micro_ros_string_utilities_set(msg_static.header.frame_id, "myframestatic");
+
+	msg.height = msg_static.height = 10;
+	msg.width = msg_static.width = 40;
+
+	msg.encoding = micro_ros_string_utilities_set(msg.encoding, "RGB");
+	msg_static.encoding = micro_ros_string_utilities_set(msg_static.encoding, "RGB");
+
+	msg.data.size = msg_static.data.size = 400;
+	for (size_t i = 0; i < msg.data.size; i++)
+	{
+		msg.data.data[i] = i;
+		msg_static.data.data[i] = i;
+	}
+	// spin executor
 	while(1){
 		rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
 		usleep(10000);
@@ -111,6 +300,10 @@ void app_main(void)
 #if defined(CONFIG_MICRO_ROS_ESP_NETIF_WLAN) || defined(CONFIG_MICRO_ROS_ESP_NETIF_ENET)
     ESP_ERROR_CHECK(uros_network_interface_initialize());
 #endif
+
+    if(ESP_OK != init_camera()) {
+        return;
+    }
 
     //pin micro-ros task in APP_CPU to make PRO_CPU to deal with wifi:
     xTaskCreate(micro_ros_task,
